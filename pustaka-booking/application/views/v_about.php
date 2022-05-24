3941f837e98faac0f2d9cf447f93b624b2093b3c
FachryF <fachryfadillah10@gmail.com>
2022-05-24 15:49:22 +0700

    Menambah Pertemuan 9

---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizki</li>
+        <li>Nama Belakang : Agung Sentosa</li>
+    </ul>
+    <br>
+    <h4>ALamat</h4>
+    <ul type="none">
+        <li>Jalan Ciapus Kp.Batugede Rt.01 Rw.14</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+    <h4>Hobby</h4>
+    <ul type="square">
+        <li>Main Komputer</li>
+        <li>Game</li>
+    </ul>
+</section>
